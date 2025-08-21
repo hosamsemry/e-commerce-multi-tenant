aa12ffe211b9115655b605166dfda9fffef74f15
hosam <semryhosam@gmail.com>
2025-08-22 02:35:55 +0300

    Registered Middleware in base settings file

--- project/settings/base.py
+++ project/settings/base.py
@@ -32,7 +32,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # later: add Tenant middleware here
+    "apps.tenancy.middleware.TenantMiddleware",
 ]
 
 ROOT_URLCONF = "project.urls"
