fcdeb9764a015d01f6e2a07ccb9c8bdbf8ab017b
hosam <semryhosam@gmail.com>
2025-08-22 02:39:05 +0300

    Made each request only see its own tenant

--- apps/tenancy/views.py
+++ apps/tenancy/views.py
@@ -3,5 +3,20 @@ from .serializers import TenantSerializer
 from rest_framework import viewsets
 
 class TenantViewSet(viewsets.ModelViewSet):
-    queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Tenant.objects.all()
+        
+        tenant = getattr(self.request, "tenant", None)
+        if tenant:
+            return Tenant.objects.filter(id=tenant.id)
+        return Tenant.objects.none()
+ 
+    
+    def perform_update(self, serializer):
+        tenant = getattr(self.request, "tenant", None)
+        if tenant:
+            serializer.save(id=tenant.id)
