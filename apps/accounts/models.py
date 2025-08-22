9ed135fcc82d35d4c06cf20a71c6743bc16faf66
hosam <semryhosam@gmail.com>
2025-08-22 16:05:44 +0300

    Created User model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from apps.tenancy.models import Tenant
 
-# Create your models here.
+class User(AbstractUser):
+    tenant = models.ForeignKey(
+        Tenant,
+        on_delete=models.CASCADE,
+        related_name="users",
+        null=True,
+        blank=True
+    )
+
+    ROLE_CHOICES = (
+        ("customer", "Customer"),
+        ("seller", "Seller"),
+        ("admin", "Admin"),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="customer")
+
+    def __str__(self):
+        return f"{self.username} ({self.tenant.name if self.tenant else 'Global'})"
