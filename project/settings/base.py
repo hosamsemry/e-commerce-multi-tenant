ac4fb0155b8a4a1ecf2a00885558f4be291e40b9
hosam <semryhosam@gmail.com>
2025-08-24 14:18:01 +0300

    Added TenantMiddleware to middleware in base settings file

--- project/settings/base.py
+++ project/settings/base.py
@@ -34,6 +34,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "apps.tenancy.middleware.TenantMiddleware",
+    "apps.accounts.middleware.TenantMiddleware",
 ]
 
 REST_FRAMEWORK = {
