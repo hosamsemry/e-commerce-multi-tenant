d1bb366cc021d90e73694f80bbe8c28418366663
hosam <semryhosam@gmail.com>
2025-08-24 15:18:07 +0300

    Registered BuyerProfile and SellerProfile in admin file

--- apps/marketplace/admin.py
+++ apps/marketplace/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BuyerProfile, SellerProfile
 
-# Register your models here.
+admin.site.register(BuyerProfile)
+admin.site.register(SellerProfile)
