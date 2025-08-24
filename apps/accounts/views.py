9d936efa6144618e800d3ef57f1242a303eeae63
hosam <semryhosam@gmail.com>
2025-08-24 14:13:07 +0300

    Refactor views to replace LoginView with CustomTokenObtainPairView  and integrate tenant handling in RegisterView

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,22 +1,20 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .serializers import RegisterSerializer, UserSerializer, CustomTokenObtainPairSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAdminUser
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 User = get_user_model()
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    def perform_create(self, serializer):
+        serializer.save(tenant=self.request.user.tenant)
 
 
-class LoginView(generics.GenericAPIView):
-    serializer_class = LoginSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 
 class ListUsersView(generics.ListAPIView):
