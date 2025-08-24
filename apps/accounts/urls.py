90d2e216ffcbefcc5ca4b607f3dc85905f4e8a71
hosam <semryhosam@gmail.com>
2025-08-24 14:15:52 +0300

    Modified login endpoint to use the new view CustomTokenObtainPairView

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ListUsersView
+from .views import *
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
+    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("users/", ListUsersView.as_view(), name="list-users"),
 ]
