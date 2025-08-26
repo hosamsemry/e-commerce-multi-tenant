1dd0078346976b93c8cb5bbbd2b708311c067604
hosam <semryhosam@gmail.com>
2025-08-26 15:30:05 +0300

    fix: remove accounts middleware to resolve tenant by domain/subdomain

--- project/settings/base.py
+++ project/settings/base.py
@@ -36,7 +36,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "apps.tenancy.middleware.TenantMiddleware",
-    "apps.accounts.middleware.TenantMiddleware",
 ]
 
 REST_FRAMEWORK = {
