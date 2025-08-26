41b542605839877a800769bf18eedcf496848b3a
hosam <semryhosam@gmail.com>
2025-08-26 15:44:53 +0300

    Register product, collection and review models in django admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["collection"]
+    inlines = [ProductImageInline]
+    list_display = ["title", "price", "collection"]
+    list_editable = ["price"]
+    search_fields = ["title"]
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+    search_fields = ["title"]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product','created_at']
+    list_filter = ['created_at']
+    search_fields = ['user__username', 'product__title']
