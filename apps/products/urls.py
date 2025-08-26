0f028556704a834620ba184c054230c9f3f2ef1f
hosam <semryhosam@gmail.com>
2025-08-26 15:25:47 +0300

    Create endpoints for Product, Collection and nested routers for ProductImage, Review

---
+++ apps/products/urls.py
@@ -0,0 +1,13 @@
+from .views import *
+from rest_framework_nested import routers
+
+router = routers.DefaultRouter()
+router.register("collections", CollectionViewSet, basename="collections")
+router.register("products", ProductViewSet, basename="products")
+router.register("products", ProductViewSet, basename="products")
+
+products_router =  routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews',ReviewViewSet,basename='product-reviews')
+products_router.register('images',ProductImageViewSet,basename='product-images')
+
+urlpatterns = router.urls + products_router.urls 
