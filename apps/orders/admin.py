d573e957bc51e2f08d98073ba14ea307336e54a1
hosam <semryhosam@gmail.com>
2025-08-28 19:22:40 +0300

    Register Order, OrderItem, Address, Cart and CartItem models in django admin

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Address, Cart, CartItem
 
-# Register your models here.
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Address)
+admin.site.register(Cart)
+admin.site.register(CartItem)
