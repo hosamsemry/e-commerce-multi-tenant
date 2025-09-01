ee247b25f34ab221791f4fa079420f0ddd0e792b
hosam <semryhosam@gmail.com>
2025-09-01 15:05:33 +0300

    Refactor: Add permissions for TenantViewSet to allow admins only to view tenants

--- apps/tenancy/views.py
+++ apps/tenancy/views.py
@@ -1,20 +1,12 @@
 from .models import Tenant
 from .serializers import TenantSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 class TenantViewSet(viewsets.ModelViewSet):
     serializer_class = TenantSerializer
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_superuser:
-            return Tenant.objects.all()
-        
-        tenant = getattr(self.request, "tenant", None)
-        if tenant:
-            return Tenant.objects.filter(id=tenant.id)
-        return Tenant.objects.none()
- 
+    permission_classes = [IsAdminUser]
+    queryset = Tenant.objects.all() 
     
     def perform_update(self, serializer):
         tenant = getattr(self.request, "tenant", None)
