429b8f79f4ceaca50225b8e430e1fe21bbcbd65b
hosam <semryhosam@gmail.com>
2025-09-01 14:47:57 +0300

    Remove unused register and login endpoints

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("users/", ListUsersView.as_view(), name="list-users"),
 ]
