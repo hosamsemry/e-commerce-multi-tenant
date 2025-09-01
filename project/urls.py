20b9326aa048ee10d8e935419e8c58fec4faee5a
hosam <semryhosam@gmail.com>
2025-09-01 14:42:29 +0300

    Include djoser urls in main urls file

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.tenancy.urls')),
     path("api/accounts/", include("apps.accounts.urls")),
+    re_path(r'^auth/', include('djoser.urls')),
     path("api/", include("apps.products.urls")),
     path("api/", include("apps.orders.urls")),
 ]
